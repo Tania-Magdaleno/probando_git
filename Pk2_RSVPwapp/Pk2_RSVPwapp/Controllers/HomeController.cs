5313f5d0b6dcdf6a94b0e471c18d92c48839d4b9
Tania <tayama_0903@hotmail.com>
2014-02-13 12:48:55 -0600

    pk2
    probando

---
+++ Pk2_RSVPwapp/Pk2_RSVPwapp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pk2_RSVPwapp.Models;
+
+namespace Pk2_RSVPwapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ?
+                "Buenos Dias!!!" : "Buena Tarde!!!";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        { 
+            //Todo: Enviar respuesta al correo ddel organizador
+            return View("Agradecimientos", guestResponse);
+        }
+
+    }
+}
